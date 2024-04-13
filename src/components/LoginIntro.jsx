a99a986b3b0d2f3e4ddc5c0b40a7f8652a79de7a
minnug <parkminnug@gmail.com>
2024-04-13 19:08:53 +0900

    Feat: 버튼, 로그인 컴포넌트 추가

---
+++ src/components/LoginIntro.jsx
@@ -0,0 +1,13 @@
+function LoginIntro() {
+  return (
+    <div className="login__intro">
+      <div className="img-wrap">
+        <img src="src/assets/images/logo_fastcampus_row_white.svg" alt="fastcampus"/>
+      </div>
+      <p className="intro-text">내일배움 아카데미</p>
+      <h2 className="intro-title">수강생용 인트라넷</h2>
+    </div>
+  );
+}
+
+export default LoginIntro;
