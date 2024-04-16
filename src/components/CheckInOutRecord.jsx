f11a82285611cfcca556b291ba40b32c82d68255
oxlzlo <yeojin58@naver.com>
2024-04-17 00:34:29 +0900

    Design: 입/퇴실시간 저장 블록 컴포넌트로 등록 및 디자인 설정

---
+++ src/components/CheckInOutRecord.jsx
@@ -0,0 +1,7 @@
+import '../styles/modules/CheckInOutRecord.scss'
+
+function CheckInOutRecord({ children, position }) {
+  return <div className={`CheckInOutRecord ${position}`}>{children}</div>
+}
+
+export default CheckInOutRecord;
