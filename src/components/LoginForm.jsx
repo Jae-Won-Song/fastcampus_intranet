a23b0af6b32448dc9e3fa04d8155bc8ae1fefcd8
minnug <parkminnug@gmail.com>
2024-04-18 21:17:38 +0900

    Fix: 충돌 파일 수정

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Button from "./Button";
 import { Link } from "react-router-dom";
@@ -17,11 +16,6 @@ function LoginForm() {
 		}
 	};
 
-=======
-import Button from "./Button";
-
-function LoginForm() {
->>>>>>> master
 	return (
 		<div className="login__form">
 			<h1 className="logo">
@@ -30,30 +24,19 @@ function LoginForm() {
 					alt="fastcampus"
 				/>
 			</h1>
-<<<<<<< HEAD
 			<form
 				className="form"
 				onSubmit={handleSubmit}>
-=======
-			<form className="form">
->>>>>>> master
 				<div className="field-wrap">
 					<div className="field">
 						<div className="input-wrap">
 							<input
 								type="text"
-<<<<<<< HEAD
 								name="email"
 								id="email"
 								placeholder="이메일(아이디)"
 								className="input"
 								value={email}
-=======
-								name="login"
-								id="login"
-								placeholder="이메일(아이디)"
-								className="input"
->>>>>>> master
 							/>
 						</div>
 					</div>
@@ -66,10 +49,7 @@ function LoginForm() {
 								id="password"
 								placeholder="비밀번호"
 								className="input"
-<<<<<<< HEAD
 								value={password}
-=======
->>>>>>> master
 							/>
 						</div>
 					</div>
@@ -79,7 +59,6 @@ function LoginForm() {
 					href="./">
 					비밀번호를 잊으셨나요?
 				</a>
-<<<<<<< HEAD
 				<Button
 					type="submit"
 					size="entire"
@@ -92,15 +71,6 @@ function LoginForm() {
 					className="Button entire black">
 					회원가입
 				</Link>
-=======
-				<Button size="entire">로그인</Button>
-				<div className="border"></div>
-				<a
-					href="./"
-					className="Button entire black">
-					회원가입
-				</a>
->>>>>>> master
 			</form>
 		</div>
 	);
