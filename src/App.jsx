e7c29a0fa72296d370ec4314bc2e0195c26a7487
Song Jae Won <jwsonghub@gmail.com>
2024-04-18 21:27:20 +0900

    merge test

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,74 @@
+import "./App.scss";
+import SideBar from "./components/SideBar";
+import Notice from "./components/Notice";
+import NoticeToy from "./components/NoticeToy";
+import NoticeSatisfaction from "./components/NoticeSatisfaction";
+import NoticeBonus from "./components/NoticeBonus";
+import NoticeEvent from "./components/NoticeEvent";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-// import LoginPage from "./pages/LoginPage";
-// import JoinPage from "./pages/JoinPage";
+import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
 import ComponentsPage from "./pages/ComponentsPage";
-import SideBar from "./components/SideBar";
 import ApplyListPage from "./pages/ApplyListPage";
 import ApplyFormPage from "./pages/ApplyFormPage";
-// import MyPage from "./pages/MyPage";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				{/* <Route
+				<Route
+					path=""
+					element={<SideBar />}
+				/>
+				<Route
+					path="Notice"
+					element={<Notice />}
+				/>
+				<Route
+					path="/NoticeToy"
+					element={<NoticeToy />}
+				/>
+				<Route
+					path="/NoticeSatisfaction"
+					element={<NoticeSatisfaction />}
+				/>
+				<Route
+					path="/NoticeBonus"
+					element={<NoticeBonus />}
+				/>
+				<Route
+					path="/NoticeEvent"
+					element={<NoticeEvent />}
+				/>
+				<Route
 					path="/login"
 					element={<LoginPage />}
 				/>
 				<Route
 					path="/join"
 					element={<JoinPage />}
-				/> */}
-				<Route element={<SideBar />}>
-					<Route
-						path="/main"
-						element={<MainPage />}
-					/>
-					<Route
-						path="/components"
-						element={<ComponentsPage />}
-					/>
-					<Route
-						path="/"
-						element={<ApplyListPage />}
-					/>
-					<Route
-						path="/applyForm"
-						element={<ApplyFormPage />}
-					/>
-					<Route
-						path="/applyList"
-						element={<ApplyListPage />}
-					/>
-				</Route>
+				/>
+				<Route
+					path="/main"
+					element={<MainPage />}
+				/>
+				<Route
+					path="/components"
+					element={<ComponentsPage />}
+				/>
+				<Route
+					path="/"
+					element={<ApplyListPage />}
+				/>
+				<Route
+					path="/applyForm"
+					element={<ApplyFormPage />}
+				/>
+				<Route
+					path="/applyList"
+					element={<ApplyListPage />}
+				/>
 			</Routes>
 		</>
 	);
