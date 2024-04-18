f9c1190b5be07741d9437f34e9ba14efc9bfe1dd
hyun-EG <codiee@naver.com>
2024-04-18 21:12:08 +0900

    충돌 해결

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 
 function SideBar() {
 	const [slidePosition, setSlidePosition] = useState(264); // 초기 슬라이드 위치
@@ -8,31 +8,6 @@ function SideBar() {
 	const handleIconClick = topPosition => {
 		setSlidePosition(topPosition); // 클릭된 아이콘의 top 위치로 슬라이드 위치 변경
 	};
-
-=======
-<<<<<<< HEAD
-import Button from "./Button";
-import { auth } from "../firebase/config";
-import { useNavigate } from "react-router-dom";
-
-function SideBar() {
-	const navigate = useNavigate();
-	const handleLogout = () => {
-		auth.signOut();
-		navigate("/login");
-	};
-	return (
-		<>
-			<div className="nav">
-				<Button
-					type="button"
-					onClick={handleLogout}>
-					로그아웃
-				</Button>
-=======
-import "../styles/components/_sidebar.scss";
-function SideBar() {
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 	return (
 		<>
 			<div className="nav">
@@ -43,21 +18,16 @@ function SideBar() {
 					/>
 				</div>
 				<div>
-<<<<<<< HEAD
 					<div
 						to="src/components/Notice.jsx"
 						className="nav-icon"
 						onClick={() => handleIconClick(264)}>
-=======
-					<div className="nav-icon">
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 						<img
 							src="src/assets/images/icon_profile.svg"
 							alt="profile"
 						/>
 					</div>
 					<div className="word">마이페이지</div>
-<<<<<<< HEAD
 					<div
 						className="slide"
 						style={{ top: slidePosition + 'px' }}></div>
@@ -66,11 +36,6 @@ function SideBar() {
 					<div
 						className="nav-icon"
 						onClick={() => handleIconClick(404)}>
-=======
-				</div>
-				<div>
-					<div className="nav-icon">
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 						<img
 							src="src/assets/images/icon_edit.svg"
 							alt="vacation"
@@ -79,7 +44,6 @@ function SideBar() {
 					<div className="word">휴가관리</div>
 				</div>
 				<div>
-<<<<<<< HEAD
 					<div
 						className="nav-icon"
 						onClick={() => handleIconClick(544)}>
@@ -89,24 +53,13 @@ function SideBar() {
 								alt="announcement"
 							/>
 						</Link>
-=======
-					<div className="nav-icon">
-						<img
-							src="src/assets/images/icon_bell.svg"
-							alt="announcement"
-						/>
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 					</div>
 					<div className="word">공지사항</div>
 				</div>
 				<div>
-<<<<<<< HEAD
 					<div
 						className="nav-icon"
 						onClick={() => handleIconClick(684)}>
-=======
-					<div className="nav-icon">
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 						<img
 							src="src/assets/images/icon_folder.svg"
 							alt="reference"
@@ -115,10 +68,6 @@ function SideBar() {
 					<div className="word">자료실</div>
 				</div>
 				<div className="nav-logout-btn">로그아웃</div>
-<<<<<<< HEAD
-=======
->>>>>>> master
->>>>>>> 861ac078b3a764d067c97fdc5bc5d707c7aab77f
 			</div>
 		</>
 	);
