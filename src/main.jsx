2709746aced3efa93b9c275a57c86bf8de04e524
Song Jae Won <jwsonghub@gmail.com>
2024-04-20 15:46:09 +0900

    배포테스트

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
 );
