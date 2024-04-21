a75288aa97e7ad0cd6da5cb8ff0fdb20f21ca9a9
Song Jae Won <jwsonghub@gmail.com>
2024-04-21 14:01:26 +0900

    이미지 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "../src/styles/main.scss";
+import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
